bf948ab01c86061e1355dc4d06fdfd148ffe4634
tanya <tatyana.chen.88@gmail.com>
2015-12-02 13:13:08 +0500

    neomed

---
+++ js/js.js
@@ -0,0 +1,22 @@
+jQuery(document).ready(function($){
+    $('.bxslider').bxSlider({
+        default: true,
+        mode: "fade",
+        auto: false
+    });
+
+    $("header .top-menu .main-top-menu > li").click(function(){
+        if ($("header .top-menu .main-top-menu > li").hasClass("tab-white")){
+            $("header .top-menu .main-top-menu > li").removeClass("tab-white").find("ul").css("display","none");
+            $("header .top-menu .main-top-menu > li").find(">a").removeClass("blue");
+        }
+        else{
+            $(this).addClass("tab-white").find("ul").toggle();
+            $(this).find(">a").addClass("blue");
+        }
+    });
+
+    $(".close").click(function(){
+        $(".top-menu").toggle();
+    })
+});
